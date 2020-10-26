a32d19f6ea1c1e65a476a5b13abea5f07f08825e
sahilsingh2402 <44718639+sahilsingh2402@users.noreply.github.com>
2020-10-26 12:09:21 +0530

    Added SumOfFibbonaci.py
    Done

---
+++ Scripts/SumOfFibonacci.py
@@ -0,0 +1,12 @@
+## Effective Algorithm for Fibonnaci.
+## To print the sum of fibonnaci numbers upto n digits.
+
+n = int(input())
+fibo_arr = [0, 1]
+
+for _ in range(2, n+1):
+    
+print(fibo_arr[-1])
+    
+    
+    
