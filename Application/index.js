d31269f456c5b65569f08a9f104ddb115aea752c
Cali Lai <cali@Calis-MacBook-Pro.local>
2020-10-27 03:07:08 -0700

    added an app.set

--- Application/index.js
+++ Application/index.js
@@ -5,22 +5,16 @@ const port = 3000
 // the below gives us a variable we can use to run all the methods that are in the express library
 const app = express();
 
+app.set('title', 'hacktoberfest');
+
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-
-
-
 
 //Start server
 app.listen(port, () => {
  //console.log() prints output
     console.log('Listening at port %d (http://localhost:%d)',port,port)
-})
-
-//render page
-app.render('index', function(err, html) => {
-    console.log(html);
 })
 
 //get time
