aad0a8ecda253d9f4e59f535b8f285e70e19bb15
iamshashank <shashankkesharwani6@gmail.com>
2020-10-28 12:47:33 +0530

    Challenge 3 completed, AreaofRectangle.java added

---
+++ Scripts/AreaOfRectangle.java
@@ -0,0 +1,9 @@
+public class AreaOfRectangle
+{
+	public static void main(String args[])
+	{
+		int b = Integer.parseInt(args[0]);
+		int h = Integer.parseInt(args[1]);
+		System.out.println("Area of triangle = " + 0.5*b*h);
+	}
+}
