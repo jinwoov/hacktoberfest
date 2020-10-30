5dc86e81076d55853c1f6c1d9620111068e08511
Kenneth Luster <51566205+KenMan79@users.noreply.github.com>
2020-10-30 15:52:14 -0600

    Challenges Pull Request

--- Application/index.js
+++ Application/index.js
@@ -10,30 +10,35 @@ app.set('title', 'hacktoberfest');
 
 // Sends a response to the request with the phrase "hello world"
 app.get('/', (req, res) => {
-  res.send('hello world')
+    res.send('hello world')
 })
 
 //Start server
 app.listen(port, () => {
- //console.log() prints output
-    console.log('Listening at port %d (http://localhost:%d)',port,port)
+    //console.log() prints output
+    console.log('Listening at port %d (http://localhost:%d)', port, port)
 })
 
 //get time
 // Suggestion: After this function, you can print a greeting according to the time.
 app.get('/time', (req, res) => {
-  	res.send(Date.now())
+    res.send(Date.now())
 })
 
 //get year
 app.get('/year', (req, res) => {
-	const date = new Date()
-  	res.send(date.getFullYear())
+    const date = new Date()
+    res.send(date.getFullYear())
 })
 
 //this code responds to $ sign to give the output.
 app.get('$', (req, res) => {
-	res.send('have a nice day!')
-  })
+    res.send('have a nice day!')
+})
 
-app.get('/nope', (req, res) => { res.send('NOPE')})
+app.get('/nope', (req, res) => { res.send('NOPE') })
+
+// Sends a response to the request with the phrase "Happy Hackathon"
+app.get('/', (req, res) => {
+    res.send('Happy Hackathon')
+})
